3d87ddb7e649e4fe004b37da0e2752c9d6fc7fbd
Stefan Åhman <stefan.ahman@apoex.se>
2016-05-20 10:25:49 +0200

    Improve logitech driver

--- lib/artoo/drivers/logitech_3d_pro.rb
+++ lib/artoo/drivers/logitech_3d_pro.rb
@@ -14,6 +14,28 @@ module Artoo
         super
       end
 
+      def handle_joystick
+        publish_joystick(0, connection.axis(0), connection.axis(1))
+        publish_turn(connection.axis(2))
+        publish_throttle(connection.axis(3))
+      end
+
+      def publish_throttle(t)
+        publish(event_topic_name("update"), "throttle", {:t => t})
+        publish(event_topic_name("throttle"), {:t => t})
+      end
+
+      def publish_turn(t)
+        publish(event_topic_name("update"), "turn", {:t => t})
+        publish(event_topic_name("turn"), {:t => t})
+      end
+
+      def publish_joystick(s, x, y)
+        publish(event_topic_name("update"), "joystick", {:x => x, :y => y, :s => s})
+        publish(event_topic_name("joystick"), {:x => x, :y => y, :s => s})
+        publish(event_topic_name("joystick_#{s}"), {:x => x, :y => y})
+      end
+
       def button_map
         BINDING_MAP[:logitech_3d_pro][:button_map]
       end
